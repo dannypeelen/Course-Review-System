8fa78350b2e8c634b4dc061f08eaa35363af6935
dannypeelen <dapeelen@gmail.com>
2025-11-13 16:39:27 -0500

    File structure build-out

---
+++ backend/coursereview/src/main/java/com/cs1530/coursereview/CoursereviewApplication.java
@@ -0,0 +1,13 @@
+package com.cs1530.coursereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursereviewApplication.class, args);
+	}
+
+}
---
+++ backend/coursereview/src/test/java/com/cs1530/coursereview/CoursereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs1530.coursereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
