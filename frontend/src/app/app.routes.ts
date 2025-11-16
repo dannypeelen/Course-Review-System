4682ad733dd282a5fe473fb966151c0d4a19807a
griffinholcombe <122371641+griffinholcombe@users.noreply.github.com>
2025-11-16 15:05:44 -0500

    Added home page component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  // TODO: Add more routes for courses, reviews, etc.
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
