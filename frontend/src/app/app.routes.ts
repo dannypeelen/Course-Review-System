adf12d8caa07cf8a0a93b92e443a6f8f03b4b050
griffinholcombe <122371641+griffinholcombe@users.noreply.github.com>
2025-12-02 13:46:06 -0500

    added course detail page and hardcoded reviews

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseDetailComponent } from './pages/course-detail/course-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'courses', component: CoursesComponent},
+  { path: 'courses', component: CoursesComponent },
+  { path: 'courses/:id', component: CourseDetailComponent },
   // TODO: Add more routes for courses, reviews, etc.
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
