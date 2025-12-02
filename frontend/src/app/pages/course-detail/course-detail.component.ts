9b0a728e95422ebd13e80256ac234403ce150e61
griffinholcombe <122371641+griffinholcombe@users.noreply.github.com>
2025-12-02 13:50:00 -0500

    fixed back to course button on course detail page

--- frontend/src/app/pages/course-detail/course-detail.component.ts
+++ frontend/src/app/pages/course-detail/course-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-course-detail',
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.scss']
 })
